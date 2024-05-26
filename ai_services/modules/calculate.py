08795c4913847a9ea308c222758f70ca0a4bf170
Daniyal Kautsar <88580903+mdaniyalk@users.noreply.github.com>
2024-05-26 12:03:56 +0700

    test-bicep

--- ai_services/modules/calculate.py
+++ ai_services/modules/calculate.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def calculate_angle(a,b,c):
-    a = np.array(a)
-    b = np.array(b)
-    c = np.array(c)
+    a = np.array([a.x, a.y])
+    b = np.array([b.x, b.y])
+    c = np.array([c.x, c.y])
 
     radians = np.arctan2(c[1]-b[1],c[0]-b[0]) - np.arctan2(a[1]-b[1],a[0]-b[0])
     angle = np.abs(radians *180.0/np.pi)
@@ -13,3 +13,16 @@ def calculate_angle(a,b,c):
         angle = 360-angle
 
     return angle
+
+
+def calc_angle(a,b,c): 
+    a = np.array([a.x, a.y])
+    b = np.array([b.x, b.y])
+    c = np.array([c.x, c.y])
+
+    ab = np.subtract(a, b)
+    bc = np.subtract(b, c)
+    
+    theta = np.arccos(np.dot(ab, bc) / np.multiply(np.linalg.norm(ab), np.linalg.norm(bc)))     # A.B = |A||B|cos(x) where x is the angle b/w A and B
+    theta = 180 - 180 * theta / 3.14    # Convert radians to degrees
+    return np.round(theta, 2)
--- ai_services/modules/pose_landmark.py
+++ ai_services/modules/pose_landmark.py
@@ -1,3 +1,4 @@
+from modules.calculate import calc_angle, calculate_angle
 import mediapipe as mp
 import numpy as np
 import cv2
@@ -22,113 +23,83 @@ class Detector:
         pass
 
 
-    def extract_keypoints_bicep_curl(self, results):
+    def extract_keypoints_bicep_curl(self, results, image = None, flag=None):
         landmarks = results.pose_landmarks.landmark
-        left_shoulder = landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER]
-        left_elbow = landmarks[self.mp_pose.PoseLandmark.LEFT_ELBOW]
-        left_wrist = landmarks[self.mp_pose.PoseLandmark.LEFT_WRIST]
-        right_shoulder = landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER]
-        right_elbow = landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW]
-        right_wrist = landmarks[self.mp_pose.PoseLandmark.RIGHT_WRIST]
+        left_shoulder = landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value]
+        left_elbow = landmarks[self.mp_pose.PoseLandmark.LEFT_ELBOW.value]
+        left_wrist = landmarks[self.mp_pose.PoseLandmark.LEFT_WRIST.value]
+        right_shoulder = landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
+        right_elbow = landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value]
+        right_wrist = landmarks[self.mp_pose.PoseLandmark.RIGHT_WRIST.value]
 
         # Calculate angle
         left_angle = calc_angle(left_shoulder, left_elbow, left_wrist)      #  Get angle 
         right_angle = calc_angle(right_shoulder, right_elbow, right_wrist)
 
-        # Visualize angle
-        cv2.putText(image,\
-                str(left_angle), \
-                    tuple(np.multiply([left_elbow.x, left_elbow.y], [640,480]).astype(int)),\
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2,cv2.LINE_AA)
-        cv2.putText(image,\
-                str(right_angle), \
-                    tuple(np.multiply([right_elbow.x, right_elbow.y], [640,480]).astype(int)),\
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2,cv2.LINE_AA)
-    
-        # Counter 
+        if image is not None:
+            cv2.putText(image,\
+                    str(left_angle), \
+                        tuple(np.multiply([left_elbow.x, left_elbow.y], [640,480]).astype(int)),\
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2,cv2.LINE_AA)
+            cv2.putText(image,\
+                    str(right_angle), \
+                        tuple(np.multiply([right_elbow.x, right_elbow.y], [640,480]).astype(int)),\
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2,cv2.LINE_AA)
+        left_count = 0
+        right_count = 0
+        
+        if flag is not None:
+            left_flag = flag[0]
+            right_flag = flag[1]
+        else:
+            left_flag = None
+            right_flag = None
+            
         if left_angle > 160:
             left_flag = 'down'
         if left_angle < 50 and left_flag=='down':
-            left_count += 1
+            left_count = 1
             left_flag = 'up'
 
         if right_angle > 160:
             right_flag = 'down'
         if right_angle < 50 and right_flag=='down':
-            right_count += 1
+            right_count = 1
             right_flag = 'up'
+        
+        if image is not None:
+            return (left_count, left_flag), (right_count, right_flag), image
+        else:
+            return (left_count, left_flag), (right_count, right_flag)
     
 
     def extract_keypoints_pushup(self, results):
         landmarks = results.pose_landmarks.landmark
         
-        shoulder = [landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-        shoulder_r = [landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-        elbow = [landmarks[self.mp_pose.PoseLandmark.LEFT_ELBOW.value].x,  landmarks[self.mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-        elbow_r = [landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,  landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-        wrist = [landmarks[self.mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[self.mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-        wrist_r = [landmarks[self.mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[self.mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-
-        nose = [landmarks[self.mp_pose.PoseLandmark.NOSE.value].x,landmarks[self.mp_pose.PoseLandmark.NOSE.value].y]
+        shoulder = landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value]
+        shoulder_r = landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
+        elbow = landmarks[self.mp_pose.PoseLandmark.LEFT_ELBOW.value]
+        elbow_r = landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value]
+        wrist = landmarks[self.mp_pose.PoseLandmark.LEFT_WRIST.value]
+        wrist_r = landmarks[self.mp_pose.PoseLandmark.RIGHT_WRIST.value]
 
 
-        left_hip = [landmarks[self.mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[self.mp_pose.PoseLandmark.LEFT_HIP.value].y]
-        right_hip = [landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-        left_knee = [landmarks[self.mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[self.mp_pose.PoseLandmark.LEFT_KNEE.value].y]
-        right_knee = [landmarks[self.mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[self.mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
-        left_ankle = [landmarks[self.mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[self.mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
-        right_ankle = [landmarks[self.mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[self.mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
-
-        midpoint_shoulder_x = (int(shoulder[0] * image_width )+ int(shoulder_r[0] * image_width))/2
-
-        midpoint_shoulder_y = (int(shoulder[1] * image_height )+ int(shoulder_r[1] * image_height))/2
-
-        midpoint_hip_x = (int(left_hip[0] * image_width )+ int(right_hip[0] * image_width))/2
-        midpoint_hip_y = (int(left_hip[1] * image_height)+ int(right_hip[1] * image_height))/2
+        left_arm_angle = calculate_angle(shoulder, elbow, wrist)
+        right_arm_angle = calculate_angle(shoulder_r, elbow_r, wrist_r)
 
 
 
-        based_mid_x = int((midpoint_shoulder_x + midpoint_hip_x)/2)
-        based_mid_y = int((midpoint_shoulder_y + midpoint_hip_y)/2)
-
-        neck_point_x = (int(nose[0] * image_width )+ int(midpoint_shoulder_x))/2
-        neck_point_y = (int(nose[1] * image_height) + int(midpoint_shoulder_y))/2
 
 
-        left_arm_angle = int(calculate_angle(shoulder, elbow, wrist))
-        right_arm_angle = int(calculate_angle(shoulder_r, elbow_r, wrist_r))
-        left_leg_angle = int(calculate_angle(left_hip, left_knee, left_ankle))
-
-        right_leg_angle = int(calculate_angle(right_hip, right_knee, right_ankle))
-
-
-        
-        left_arm_length = np.linalg.norm(np.array(shoulder) - np.array(elbow))
-
-        # ppm = 10.8
-
-        # left_arm_motion = left_arm_angle* left_arm_length
-
-        # left_arm_motion = left_arm_motion/ppm
-
-        #newpoint_left = [left_hip[0] +5,right_hip[0] +5]
-
-        mid_point_x = (int(left_hip[0] * image_width )+ int(right_hip[0] * image_width))/2
-        mid_point_y = (int(left_hip[1] * image_height)+ int(right_hip[1] * image_height))/2
-
-        #cv2.circle(image,(int(mid_point_x) ,int(mid_point_y +30 )),15,(0,255,255),-1)
-
-
-
-        if left_arm_angle > 160:
+        if left_arm_angle > 160 or right_arm_angle > 160:
             up_pos = 'Up'
             display_pos = 'Up'
 
-        if left_arm_angle < 110 and up_pos == 'Up':
+        if (left_arm_angle < 110 or right_arm_angle < 110) and up_pos == 'Up':
             down_pos = 'Down'
             display_pos = 'Down'    
 
-        if left_arm_angle > 160 and down_pos == 'Down':
+        if (left_arm_angle > 160 or right_arm_angle > 160) and down_pos == 'Down':
 
             pushup_pos = "up"
             display_pos = "up"
@@ -139,45 +110,5 @@ class Detector:
             pushup_pos = None 
 
         
-        #visulaize the angle
-        cv2.line(image,(int(shoulder[0]* image_width),int(shoulder[1]* image_height)),(int(neck_point_x),int(neck_point_y)),(line_color),3)
-
-        cv2.line(image,(int(shoulder_r[0]* image_width),int(shoulder_r[1]* image_height)),(int(neck_point_x),int(neck_point_y)),(line_color),3)
-
-        cv2.line(image,(int(shoulder[0]* image_width),int(shoulder[1]* image_height)),(int(elbow[0]* image_width),int(elbow[1]* image_height)),(line_color),3)
-        cv2.line(image,(int(shoulder_r[0]* image_width),int(shoulder_r[1]* image_height)),(int(elbow_r[0]* image_width),int(elbow_r[1]* image_height)),(line_color),3)
-
-        
-            
-
-            #neck to mid point
-        cv2.line(image,(int(neck_point_x),int(neck_point_y)),(int(based_mid_x),int(based_mid_y)),(line_color),3,cv2.LINE_4)
-
-            #mid to hips
-        cv2.line(image,(int(based_mid_x),int(based_mid_y)),(int(left_hip[0] * image_width ),(int(left_hip[1] * image_height))),(line_color),3,cv2.LINE_8)
-
-        cv2.line(image,(int(based_mid_x),int(based_mid_y)),(int(right_hip[0] * image_width ),(int(right_hip[1] * image_height))),(line_color),3,cv2.LINE_8)
-
-
-            ##neck point
-
-
-        cv2.circle(image,(int(neck_point_x),int(neck_point_y)),4,(line_color),5)
-
-            #create new circles at that place
-        cv2.circle(image,(int(shoulder[0]* image_width),int(shoulder[1]* image_height)),4,(line_color),3)
-        cv2.circle(image,(int(shoulder_r[0]* image_width),int(shoulder_r[1]* image_height)),4,(line_color),3)
-            #mid point
-        cv2.circle(image,(int(based_mid_x),int(based_mid_y)),4,(line_color),5)
-
-
-
-        cv2.rectangle(image,(image_width,0),(image_width-330,250),(0,0,0),-1)
-        cv2.putText(image,'Angles',(image_width-300,30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,255),2)
-        cv2.putText(image, 'Left Elbow Angle: ' + str(left_arm_angle), (image_width-290, 70), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(image, 'Right Elbow Angle: ' + str(right_arm_angle), (image_width-290, 110), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(image, 'Left Knee Angle: ' + str(left_leg_angle), (image_width-290, 150), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(image, 'Right Knee Angle: ' + str(right_leg_angle), (image_width-290, 190), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
-
         cv2.putText(image, 'Push Up Counter: ' + str(push_up_counter), (image_width-320, 240), cv2.FONT_HERSHEY_COMPLEX, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
         
